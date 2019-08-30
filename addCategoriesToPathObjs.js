827147dc3255e6983a01616d85d695bf93aae4d5
abc123 <abc123@gmail.com>
2019-08-30 22:09:06 +1200

    Generate categories from path names

---
+++ addCategoriesToPathObjs.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+async function addCategoriesToPathObjs(arr){
+
+	let new_arr = []
+	await arr.forEach(obj =>{
+		obj.categories = []
+		
+		let dirname		= path.dirname(obj.path)
+		let extname		= path.extname(obj.path)
+		let basename	= path.basename(obj.path, extname)
+
+		let path_cats = dirname.split('/')
+
+		if (path_cats.length > 0) {
+			obj.categories.push(...path_cats)
+		}
+		if (extname){
+			obj.categories.push(extname)
+		} if (basename){
+		obj.categories.push(basename)
+		}
+
+		new_arr.push(obj)
+
+	})
+	
+	return new_arr
+
+}
+
+module.exports = addCategoriesToPathObjs
--- index.js
+++ index.js
@@ -1,10 +1,68 @@
-const fs = require('fs')
+const fs 											= require('fs')
+const path 										= require('path')
+const yaml 										= require('js-yaml')
+const config_path							= './restruct.yml'
+const addCategoriesToPathObjs	= require('./addCategoriesToPathObjs.js')
+var config
 
-paths = fs.readdirSync('./')
+try {
+	config = yaml.safeLoad(fs.readFileSync(config_path, 'utf8'))
+//	console.log(config)
+} catch (e) {}
 
-paths.forEach( path => {
-	fs.stat( path, (err, stats) => {
-		console.log(stats.isDirectory())
+var paths_objs = []
+
+async function readDir(root_path, depth){
+
+	let paths = fs.readdirSync(root_path)
+
+	await paths.forEach( async file_path => {
+		file_path = path.join(root_path, file_path)
+		let stats = fs.statSync( file_path )
+		let type = stats.isDirectory() ? 'dir' : 'file'
+//		console.log(`${file_path} at ${depth}`)
+
+		let obj = { path: file_path, type }
+
+//					console.log("config.mindepth: " + config.mindepth)
+//					console.log("depth: " + depth)
+		if ( depth >= config.mindepth ){
+			paths_objs.push(obj)
+		} else {
+//			console.log(`exclude file ${root_path}`)
+		}
+
+		if (obj.type === 'dir'){
+			await statPaths(obj.path, depth)
+		}
+
 	})
-})
 
+
+}
+
+async function statPaths(root_path, depth=0){
+	
+	depth++
+//	console.log(depth)
+
+	if ( config.ignore.includes(root_path) ){
+//		console.log(`exclude path ${root_path}`)
+	}	else {
+		readDir(root_path, depth)
+	}
+
+	return paths_objs
+
+}
+
+statPaths('./')
+	.then( obj => {
+		console.log( obj )
+		obj = addCategoriesToPathObjs(obj)
+		return obj
+	})
+	.then( obj => {
+		console.log( obj )
+		return obj
+	})
